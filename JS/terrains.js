859453b420a13fbacbc96571fea0c857d93cc245
Fontaine Francois <fontaine.francois1990@gmail.com>
2015-12-04 13:29:51 +0100

    appli foot 13h30 4/12

--- JS/terrains.js
+++ JS/terrains.js
@@ -10,30 +10,32 @@ var numerotire6;
 function getRandom(min,max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
-
-function tirageAuSort() {
-
+function tirage_terrain1() {
            numeroTire1 = getRandom(0,arr.length);
-           while (numeroTire2 == numeroTire1) {
-             numeroTire2 = getRandom(0,arr.length);
-           }
-
-
            numeroTire2 = getRandom(0,arr.length);
-
-           
-           numeroTire3 = getRandom(0,arr.length);
-           numeroTire4 = getRandom(0,arr.length);
-           numeroTire5 = getRandom(0,arr.length);
-           numeroTire6 = getRandom(0,arr.length);
-
-
-
-
+              while (numeroTire2 == numeroTire1) {
+                numeroTire2 = getRandom(0,arr.length);
+              }
           document.getElementById("equipe1").innerHTML = arr[numeroTire1];
           document.getElementById("equipe2").innerHTML = arr[numeroTire2];
+}
+
+function tirage_terrain2() {
+           numeroTire3 = getRandom(0,arr.length);
+           numeroTire4 = getRandom(0,arr.length);
+              while (numeroTire4 == numeroTire3) {
+                numeroTire4 = getRandom(0,arr.length);
+              }
           document.getElementById("equipe3").innerHTML = arr[numeroTire3];
           document.getElementById("equipe4").innerHTML = arr[numeroTire4];
+}
+
+function tirage_terrain3() {
+          numeroTire5 = getRandom(0,arr.length);
+          numeroTire6 = getRandom(0,arr.length);
+          while (numeroTire6 == numeroTire5) {
+            numeroTire6 = getRandom(0,arr.length);
+          }
           document.getElementById("equipe5").innerHTML = arr[numeroTire5];
           document.getElementById("equipe6").innerHTML = arr[numeroTire6];
 }
